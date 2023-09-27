2d5bb3b781d8984f93244373f0c24d986e60c1b6
mamangopik <hidayat.12.software@gmail.com>
2023-09-27 14:39:36 +0700

    bug fix for dump-adsb-raw.py

--- dump-adsb-raw.py
+++ dump-adsb-raw.py
@@ -62,7 +62,7 @@ try:
                 # print(parsed_message)
                 # print(">>>>>>",distance)
                 data_buffer['distance'].append(str(json.dumps(distance)))
-        if len(data_buffer['distance'])>20:
+        if len(data_buffer['distance'])>2:
             try:
                 converted_datetime = str(unix_timestamp_to_datetime(time.time()))
                 payload = {
