a2b922ed3cf53e0230f7dd185c8438a521f27e49
mamangopik <hidayat.12.software@gmail.com>
2023-10-05 15:49:29 +0700

    add net_priority_manager.py

---
+++ net_priority_manager.py
@@ -0,0 +1,80 @@
+import subprocess
+import time
+
+# Define the list of network interfaces to test
+interfaces = ["eth0", "wlan0", "wwan0"]
+# Define the test server to ping
+test_server = "8.8.8.8"
+
+ping_result = {}
+
+# Function to run ping and get metrics
+def get_ping_metrics(interface):
+    try:
+        ping_result = subprocess.check_output(
+            ["ping", "-I", interface, "-c", "4", "-q", test_server],
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        )
+
+        # Extract RTT and packet loss from the ping result
+        rtt_line = [line for line in ping_result.split("\n") if "rtt min/avg/max/mdev" in line][0]
+        packet_loss_line = [line for line in ping_result.split("\n") if "packet loss" in line][0]
+
+        rtt = float(rtt_line.split("/")[4])
+        packet_loss = float(packet_loss_line.split(",")[2].split("%")[0])
+
+        return rtt, packet_loss
+    except subprocess.CalledProcessError as e:
+        # Handle errors, e.g., if ping fa3.ils
+        return None, None
+
+def switch_metric(roll):
+    print("switch metric")
+    metric_val = []
+    if roll == 0:  #priority eth0
+        metric_val = [200,300.400]
+    if roll == 1: #priority wlan0
+        metric_val = [400,200,300]
+    if roll == 2: #priority wwan0
+        metric_val = [300,400,200]
+    try:
+        subprocess.run(["ifmetric", 'eth0', str(metric_val[0])], check=True)
+        time.sleep(1)
+        subprocess.run(["ifmetric", 'wlan0', str(metric_val[1])], check=True)
+        time.sleep(1)
+        subprocess.run(["ifmetric", 'wwan0', str(metric_val[2])], check=True)
+        time.sleep(1)
+    except:
+        pass
+
+
+def metric_rule():
+    if ping_result['eth0']: #priority eth0
+        switch_metric(0)
+    else:
+        if ping_result['wlan0']: #priority wlan0
+            switch_metric(1)
+        else:
+            if ping_result['wwan0']: #priority wwan0
+                switch_metric(2)
+            else:
+                print("not connected to internet")
+
+for interface in interfaces:
+    ping_result[interface] = False
+
+
+while 1:
+    for interface in interfaces:
+        rtt, packet_loss = get_ping_metrics(interface)
+        if rtt is not None and packet_loss is not None:
+            ping_result[interface] = True
+        if rtt == None or packet_loss == None:
+            ping_result[interface] = False
+
+    print(ping_result)
+    try:
+        metric_rule()
+    except:
+        print("something went wrong")
